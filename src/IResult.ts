34a44a1334ad4fedbb17af9f7ae4ff67290cfa48
dennis7 <dynasdaq@gmail.com>
2017-09-28 07:34:47 +0200

    add error it IErrror

--- src/IResult.ts
+++ src/IResult.ts
@@ -1,19 +1,19 @@
 export interface IResult {
   success?: boolean;      // in case of progress, this is undefined
-  log?: ILog;
+  data?: any;             // the output of the result
+  log?: ILog;             // infos...
   info?: ILog;
   warn?: ILog;
   error?: ILog;
   debug?: ILog;
-  data?: any;
 }
 
 export interface ILog {
-  code?: number;
-  section?: string;
-  message?: string;
-  data?: any;
-  parent?: IResult;
+  code?: number | string; // code of the info/error/...
+  section?: string;       // section of the application, action
+  message?: string;       // a meaningful dev/debug message
+  data?: any;             // optional data of this section/action
+  parent?: IResult;       // in case of bubble, the parent of it
 }
 
 export interface IWarn extends ILog {
@@ -23,6 +23,7 @@ export interface IInfo extends ILog {
 }
 
 export interface IError extends ILog {
+  error: any;             // the raw error object
 }
 
 export interface IDebug extends ILog {
