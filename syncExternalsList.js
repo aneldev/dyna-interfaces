7ab03d31f7ecde43ded2a4d44bc5a61ca9fb4b00
Dennis Teo <dennis@anel.co>
2017-11-15 07:58:15 +0100

    clean up

--- syncExternalsList.js
+++
@@ -1,4 +0,0 @@
-module.exports.syncExternalsList = [
-  '/mnt/c/dev/Dyna-repos/dyna-react-server-client-app/client/node_modules/dyna-interfaces',
-  '/mnt/c/dev/Dyna-repos/dyna-react-server-client-app/server/node_modules/dyna-interfaces',
-];
